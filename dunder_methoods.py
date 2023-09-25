25bbe40a1b9840b8de575cc353f5939014773dd7
SaF_RON <safron.d.o@gmail.com>
2023-09-25 23:21:38 +0300

    Create class Point

---
+++ dunder_methoods.py
@@ -0,0 +1,24 @@
+class Point:
+    MIN_COORD = 0
+    MAX_COORD = 100
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_coord(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_bound(self, left):
+        self.MIN_COORD = left
+
+
+
+if __name__ == '__main__':
+    pt1 = Point(1, 2)
+    pt2 = Point(10, 20)
+    print(pt1.MAX_COORD)
+    pt1.set_bound(-100)
+    print(pt1.__dict__)
+    print(pt2.__dict__)
